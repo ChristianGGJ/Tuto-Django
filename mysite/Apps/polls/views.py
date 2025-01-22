dcd79d610b36c23b54fc51a79439f11ee9118538
Christianggj02 <98190852+Christianggj02@users.noreply.github.com>
2025-01-21 18:56:25 -0600

    parte 6 del tutorial

--- mysite/Apps/polls/views.py
+++ mysite/Apps/polls/views.py
@@ -1,4 +1,4 @@
-from datetime import timezone
+from django.utils import timezone
 from django.db.models import F
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
